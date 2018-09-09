d8fad4d243f25e957708fe9ef83fa5a7213c5c8a
eujy <rxu.1094.9257@gmail.com>
2018-09-09 14:00:28 +0900

    color

--- randIcon/ViewController.swift
+++ randIcon/ViewController.swift
@@ -9,10 +9,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var key: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let kbToolBar = UIToolbar(frame: CGRect(x: 0, y: 0, width: 320, height: 40))
+        kbToolBar.barStyle = UIBarStyle.default  // スタイルを設定
+        kbToolBar.sizeToFit()  // 画面幅に合わせてサイズを変更
+        // スペーサー
+        let spacer = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.flexibleSpace, target: self, action: nil)
+        // 閉じるボタン
+        let commitButton = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.done, target: self, action: #selector(self.commitButtonTapped))
+        kbToolBar.items = [spacer, commitButton]
+        key.inputAccessoryView = kbToolBar
+    
+    }
+    @objc func commitButtonTapped() {
+        self.view.endEditing(true)
     }
 
     override func didReceiveMemoryWarning() {
