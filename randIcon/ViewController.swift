8a0a16db6766e06f038f861be7c8178e74e52945
eujy <rxu.1094.9257@gmail.com>
2018-09-14 01:23:50 +0900

    save not yet

--- randIcon/ViewController.swift
+++ randIcon/ViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class ViewController: UIViewController {
     @IBOutlet weak var key: UITextField!
     @IBOutlet weak var gazou: UIImageView!
+    var IMG: UIImage!
     @IBAction func DrawPic(_ sender: Any) {
         // Screen Size の取得
         let screenWidth = self.gazou.bounds.width
@@ -21,14 +22,61 @@ class ViewController: UIViewController {
         let testDraw = Draw(frame: CGRect(x: screenX, y: screenY,
                                           width: screenWidth, height: screenHeight))
         self.view.addSubview(testDraw)
+        
+        // キャプチャする範囲を取得する
+        let rect = CGRect(x: screenX, y: screenY,
+                          width: screenWidth, height: screenHeight)
+        
+        // ビットマップ画像のcontextを作成する
+        UIGraphicsBeginImageContextWithOptions(rect.size, false, 0.0)
+        let context : CGContext = UIGraphicsGetCurrentContext()!
+        
+        // view内の描画をcontextに複写する
+        view.layer.render(in: context)
+        
+        // contextのビットマップをUIImageとして取得する
+        let image : UIImage = UIGraphicsGetImageFromCurrentImageContext()!
+        
+        // contextを閉じる
+        UIGraphicsEndImageContext()
+        
+        IMG = image
+       
     }
     
     @IBAction func Save(_ sender: Any) {
-//        // UIImage の画像をカメラロールに画像を保存
-//        UIImageWriteToSavedPhotosAlbum(gazou, self, #selector(self.showResultOfSaveImage(_:didFinishSavingWithError:contextInfo:)), nil)
+        
+
+        // UIImage の画像をカメラロールに画像を保存
+//        UIImageWriteToSavedPhotosAlbum(image, self, #selector(self.showResultOfSaveImage(_:didFinishSavingWithError:contextInfo:)), nil)
+        if IMG != nil {
+            UIImageWriteToSavedPhotosAlbum(IMG, self,
+                                           #selector(self.showResultOfSaveImage(_:didFinishSavingWithError:contextInfo:)), nil)
+        }
+        else{
+           print("save failed")
+        }
     }
     
-    
+    // 保存を試みた結果をダイアログで表示
+    @objc func showResultOfSaveImage(_ image: UIImage, didFinishSavingWithError error: NSError!, contextInfo: UnsafeMutableRawPointer) {
+        
+        var title = "保存完了"
+        var message = "カメラロールに保存しました"
+        
+        if error != nil {
+            title = "エラー"
+            message = "保存に失敗しました"
+        }
+        
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        
+        // OKボタンを追加
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        
+        // UIAlertController を表示
+        self.present(alert, animated: true, completion: nil)
+    }
     
     
     override func viewDidLoad() {
